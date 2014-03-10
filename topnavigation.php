7c84fecab43dcfc5576c3e56598863299f8fdfcd
heromiya <heromiya@heromiya.net>
2014-03-10 22:43:39 +0800

    Initial commit

---
+++ topnavigation.php
@@ -0,0 +1,65 @@
+	  <script language="JavaScript">
+		$(document).ready(function(){
+			$('#topmenu li.headlink').hover(
+				function() { $('ul', this).css('display', 'block'); },
+				function() { $('ul', this).css('display', 'none'); });
+		});
+	  </script>
+  
+		<ul id="topmenu">
+		<li class="headlink">
+			<a href="#">
+				<img src="<?php echo path_to_theme().'/images/isprssc.png'?>" border="0">
+			</a>
+			<ul>
+				<li><a href="http://www.isprs-sc.org/page/get/1">What is SC?</a></li>
+				<li><a href="http://www.isprs-sc.org/page/get/2">General Activities</a></li>
+				<li><a href="http://www.isprs-sc.org/members/board">Board Members</a></li>
+				<li><a href="http://www.isprs-sc.org/members/coordinators">Regional Coordinators</a></li>
+				<li><a href="http://www.isprs-sc.org/members/map">Members Map</a></li>
+				<li><a href="http://www.isprs-sc.org/contact">Contact Us</a></li>
+			</ul>
+		</li>
+		<li class="headlink">
+			<a href="http://www.isprs-sc.org/message_board">
+				<img src="<?php echo path_to_theme().'/images/message_boards.png'?>" border="0">
+			</a>
+		<ul><li><a href="http://www.isprs-sc.org/message_board/board/1">Announcements</a></li><li><a href="http://www.isprs-sc.org/message_board/board/2">ISPRS Related Events</a></li><li><a href="http://www.isprs-sc.org/message_board/board/3">Scholarships</a></li><li><a href="http://www.isprs-sc.org/message_board/board/5">Job Opportunities</a></li><li><a href="http://www.isprs-sc.org/message_board/board/6">Summer Schools</a></li></ul></li>
+	 <li class="headlink">
+		<a href="http://www.isprs-sc.org/events">
+		  <img src="<?php echo path_to_theme().'/images/events.png'?>" border="0">
+		</a><ul><li><a href="http://www.isprs-sc.org/events/cat/33">ISPRS SC Events</a></li><li><a href="http://www.isprs-sc.org/events/cat/34">ISPRS Events</a></li><li><a href="http://www.isprs-sc.org/events/cat/35">Other Events</a></li></ul>
+	  </li>
+	 <li class="headlink">
+		<a href="http://www.isprs-sc.org/members/directory">
+		  <img src="<?php echo path_to_theme().'/images/members.png'?>" border="0">
+		</a>
+	  </li>
+	  <li class="headlink">
+		<a href="http://www.isprs-sc.org/gallery">
+		  <img src="<?php echo path_to_theme().'/images/gallery.png'?>" border="0">
+		</a>
+	  </li>
+	  <li class="headlink">
+		<a href="#">
+		  <img src="<?php echo path_to_theme().'/images/materials.png'?>" border="0">
+		</a><ul>
+		<li><a href="http://www.isprs-sc.org/newsletters">Newsletters</a></li><li><a href="http://www.isprs-sc.org/materials/cat/11">Summer Schools</a></li><li><a href="http://www.isprs-sc.org/materials/cat/12">Proceedings</a></li><li><a href="http://www.isprs-sc.org/materials/cat/13">Reports</a></li><li><a href="http://www.isprs-sc.org/materials/cat/14">Other Documents</a></li><li><a href="http://www.isprs-sc.org/materials/cat/21">PR Materials</a></li></ul>
+	  </li>
+	  <li class="headlink">
+		<a href="#">
+		  <img src="<?php echo path_to_theme().'/images/links.png'?>" border="0">
+		</a>
+		<ul>
+					<li><a href="http://www.isprs.org/">ISPRS Home Page</a></li>
+					<li><a href="http://www.commission6.isprs.org/wg5/index.html" target="_blank">Working Group VI/5</a></li>
+			<li><a href="http://www.isprs-sc.org/about/page/5">ISPRS SC Links</a></li>
+			<li><a href="http://www.isprs.org/education/" target="_blank">ISPRS Education</a></li>
+					<li><a href="http://www.isprs.org/publications/archives.aspx" target="_blank">ISPRS Archive</a></li>
+					<li><a href="http://www.isprs.org/publications/highlights.aspx" target="_blank">ISPRS Highlights</a></li>
+					<li><a href="http://www.isprs.org/calendar/default.aspx" target="_blank">ISPRS Calendar</a></li>
+					<li><a href="https://www.facebook.com/groups/isprssc/" target="_blank">Facebook ISPRS SC Group</a></li>
+					<li><a href="http://www.linkedin.com/groups/ISPRS-Student-Consortium-4510838" target="_blank">LinkedIn ISPRS SC Group</a></li>
+			</ul>
+	  </li>
+	  </ul>
